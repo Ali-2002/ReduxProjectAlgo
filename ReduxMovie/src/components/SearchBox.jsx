5ca13fc3ca13c6100769de9d4b86661ed564b91b
Ali-2002 <ali.aliquliyev@inbox.ru>
2022-11-26 19:17:53 +0400

    sliderr

--- ReduxMovie/src/components/SearchBox.jsx
+++ ReduxMovie/src/components/SearchBox.jsx
@@ -6,31 +6,37 @@ const SearchBox = () => {
   const { searchMovies } = useSelector((state) => state.searchMovies);
   console.log(searchMovies);
   const dispatch = useDispatch();
-  useEffect(() => {
+  // useEffect(() => {
+  //   dispatch(getMovies(search));
+  // }, [search]);
+  const submitHandler=(e)=>{
+    e.preventDefault();
     dispatch(getMovies(search));
-  }, [search]);
+  }
   return (
     <>
-      <div className="form-control flex flex-row flex-wrap justify-center gap-6">
-        <div>
-          <label className="input w-1/4 shadow-xl">
-            <input
-              type="text"
-              onChange={(e) => setSearch(e.target.value.trim())}
-              value={search}
-              placeholder="Enter film..."
-              className="input input-bordered w-60 max-w-xs focus:outline-none p-2 rounded-lg"
-            />
-          </label>
+      <form onSubmit={submitHandler}>
+        <div className="form-control flex flex-row flex-wrap justify-center gap-6">
+          <div>
+            <label className="input w-1/4 shadow-xl">
+              <input
+                type="text"
+                onChange={(e) => setSearch(e.target.value.trim())}
+                value={search}
+                placeholder="Enter film..."
+                className="input input-bordered w-60 max-w-xs focus:outline-none p-2 rounded-lg"
+              />
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="btn bg-teal-300 border-none text-xl text-white p-1 rounded-lg"
+            disabled={!search}
+          >
+            Search
+          </button>
         </div>
-        <button
-          onClick={() => getMovies(search)}
-          className="btn bg-teal-300 border-none text-xl text-white p-1 rounded-lg"
-          disabled={!search}
-        >
-          Search
-        </button>
-      </div>
+      </form>
     </>
   );
 };
