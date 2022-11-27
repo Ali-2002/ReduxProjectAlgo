7e291c240efed2a77857387daea22a9ee449a343
Ali-2002 <ali.aliquliyev@inbox.ru>
2022-11-28 02:54:22 +0400

    contuniue

--- ReduxMovie/src/components/Favorites.jsx
+++ ReduxMovie/src/components/Favorites.jsx
@@ -11,34 +11,40 @@ const Favorites = () => {
 
   return (
     <>
-      <div className="w-full bg-orange-500 rounded-lg p-6">
-        <h1 className="mb-5">Favorite Films</h1>
-
+      <div className="w-full flex flex-col justify-center items-center gap-3 bg-orange-500 rounded-lg p-6">
+        <h1 className="mb-5 text-6xl font-semibold text-white text-center">
+          Favorite Films
+        </h1>
         <input
           type="text"
           placeholder="Enter list name..."
           value={listName}
           onChange={(e) => setListName(e.target.value)}
           disabled={linkActive}
-          className="input input-bordered input-primary w-full max-w-xs"
+          className="input input-bordered input-primary w-full max-w-xs p-3 rounded-lg text-2xl focus:outline-none"
         />
-
-        <ul>
-          {moviesList.length > 0
-            ? moviesList.map((fav) => {
-                return (
-                  <li key={fav.imdbID} className="">
-                    {fav.Title}
-                    <button
-                      onClick={() => dispatch(removeFromList(fav.imdbID))}
+        {moviesList.length > 0 ? (
+          <ul className="bg-slate-500 p-7 rounded-lg shadow-2xl flex flex-col gap-4">
+            {moviesList.length > 0
+              ? moviesList.map((fav) => {
+                  return (
+                    <li
+                      key={fav.imdbID}
+                      className="text-white text-xl flex justify-center items-center"
                     >
-                      <TiDeleteOutline />
-                    </button>
-                  </li>
-                );
-              })
-            : null}
-        </ul>
+                      {fav.Title}
+                      <button
+                        className="ml-3 my-auto hover:cursor-pointer"
+                        onClick={() => dispatch(removeFromList(fav.imdbID))}
+                      >
+                        <TiDeleteOutline size={28} />
+                      </button>
+                    </li>
+                  );
+                })
+              : null}
+          </ul>
+        ) : null}
       </div>
     </>
   );
