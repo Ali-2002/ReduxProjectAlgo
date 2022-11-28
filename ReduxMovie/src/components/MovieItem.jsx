1e4b6a422e60e79e558f843f226542bdda30e123
Ali-2002 <ali.aliquliyev@inbox.ru>
2022-11-28 10:04:57 +0400

    ...

--- ReduxMovie/src/components/MovieItem.jsx
+++ ReduxMovie/src/components/MovieItem.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToList } from "../store/actions/actions";
+import { RiHeartAddLine } from "react-icons/ri";
+import { MdOutlineDoneOutline } from "react-icons/md";
 const MovieItem = ({ movie, disabled }) => {
   const dispatch = useDispatch();
   const { linkActive } = useSelector((state) => state.linkActive);
   return (
     <>
-      <div className="flex flex-col justify-center item-center text-center p-5 bg-[#171919] shadow-lg backdrop-4 rounded-lg">
+      <div className="flex flex-col justify-center item-center text-center p-5 bg-[#202020] rounded-lg">
         <img
           className="w-64 flex justify-center rounded-lg shadow-lg m-auto"
           src={movie.Poster}
           alt={movie.Title}
         />
-        <div className="w-72 text-center">
+        <div className="w-72 text-center mb-3">
           <h3 className="text-white text-xl mt-4">{movie.Title}</h3>
           <p className="text-white text-xl">{movie.Year}</p>
         </div>
@@ -20,9 +22,13 @@ const MovieItem = ({ movie, disabled }) => {
           disabled={disabled || linkActive}
           onClick={() => dispatch(addToList(movie.imdbID))}
           type="button"
-          className="inline-block text-white bg-[#44413f]  ease-in-out text-3xl m-auto px-5 py-3 rounded-md w-40 "
+          className="flex justify-center items-center"
         >
-          {!disabled ? "Add" : "Done"}
+          {!disabled ? (
+            <RiHeartAddLine size={50} style={{ color: "red" }} />
+          ) : (
+            <MdOutlineDoneOutline size={50} style={{ color: "white" }} />
+          )}
         </button>
       </div>
     </>
